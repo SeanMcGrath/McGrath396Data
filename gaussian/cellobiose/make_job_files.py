f9ac0bc276b26056278a3c6be187f916c61f2676
SeanMcGrath <srmcgrat@umass.edu>
2016-01-20 15:01:51 -0500

    made gaussian job files

---
+++ gaussian/cellobiose/make_job_files.py
@@ -0,0 +1,17 @@
+import os
+from glob import glob
+
+with open('dft.job') as template_file:
+    template_lines = template_file.readlines()
+
+top_level = os.path.abspath('.')
+for file in glob('**/**/*.com'):
+    folder = '/'.join(os.path.abspath(file).split('/')[:-1])
+    os.chdir(folder)
+    com_file = glob('*.com')[0]
+    with open('dft.job', 'w') as new_file:
+        for line in template_lines:
+            if 'YOUR-COMMAND' in line:
+                line = 'g09run ' + com_file + '\n'
+            new_file.write(line)
+    os.chdir(top_level)
