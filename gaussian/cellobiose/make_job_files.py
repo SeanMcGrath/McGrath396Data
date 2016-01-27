782c819396dd52fb3ed8a7ce89ae2a69613517e0
SeanMcGrath <srmcgrat@umass.edu>
2016-01-27 17:45:13 -0500

    organized folders to make multiple snapshots at each time step possible, generated second set of cellobiose units

--- gaussian/cellobiose/make_job_files.py
+++ gaussian/cellobiose/make_job_files.py
@@ -5,10 +5,11 @@ with open('dft.job') as template_file:
     template_lines = template_file.readlines()
 
 top_level = os.path.abspath('.')
-for file in glob('**/**/*.com'):
+for file in glob('**/**/**/*.com'):
     folder = '/'.join(os.path.abspath(file).split('/')[:-1])
     os.chdir(folder)
     com_file = glob('*.com')[0]
+    print(com_file)
     with open('dft.job', 'w') as new_file:
         for line in template_lines:
             if 'YOUR-COMMAND' in line:
