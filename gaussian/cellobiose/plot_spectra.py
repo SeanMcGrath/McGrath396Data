8cef66f17412a0eac02c1e26b31ae7ac3fec9d24
SeanMcGrath <srmcgrat@umass.edu>
2016-01-29 14:58:08 -0500

    fixed 280/6000 com file

--- gaussian/cellobiose/plot_spectra.py
+++ gaussian/cellobiose/plot_spectra.py
@@ -1,10 +1,17 @@
 import os
+import sys
 import raman
 import functools
 import matplotlib
 import seaborn as sns
 from glob import glob
 import matplotlib.pyplot as plt
+
+# Choose stacked or normal plot
+if '-s' in sys.argv or '--stacked' in sys.argv:
+    stacked = True
+else:
+    stacked = False
 
 plt.axis([900, 1250, 0, 35])
 sns.set_context('poster')
@@ -20,13 +27,17 @@ for dir in temp_dirs:
     max_x = max([s.x_array()[-1] for s in spectra]) 
     x_array = raman.util.linspace(0, max_x, 10000)
 
-    plt.plot(x_array, [avg_function(x) for x in x_array], label=dir + 'K')
+    if !stacked:
+        plt.plot(x_array, [avg_function(x) for x in x_array], label=dir + 'K')
+    else:
+        plt.plot(x_array, [avg_function(x) + y_offset for x in x_array], label=dir + 'K')
+        y_offset += 8
     plt.legend()
 
     os.chdir('..')
 
 plt.xlabel('Frequency (cm$^{-1}$)', fontsize=20)
-plt.ylabel('Raman Intensity (arb. units)', fontsize=20)
+plt.ylabel('Raman Activity (arb. units)', fontsize=20)
 plt.gca().tick_params(axis='both', which='major', labelsize='16')
 
 plt.show()
