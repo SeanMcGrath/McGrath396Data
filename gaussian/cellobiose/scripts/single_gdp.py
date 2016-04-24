c14398ea73d30a77368cf059e449ab1e364386c4
SeanMcGrath <srmcgrat@umass.edu>
2016-04-24 18:56:02 -0400

    revised single temp graphs and scripts;

--- gaussian/cellobiose/scripts/single_gdp.py
+++ gaussian/cellobiose/scripts/single_gdp.py
@@ -7,51 +7,14 @@ import seaborn as sns
 from glob import glob
 import matplotlib.pyplot as plt
 
-def suplabel(axis,label,label_prop=None,
-             labelpad=5,
-             ha='center',va='center'):
-    ''' Add super ylabel or xlabel to the figure
-    Similar to matplotlib.suptitle
-    axis       - string: "x" or "y"
-    label      - string
-    label_prop - keyword dictionary for Text
-    labelpad   - padding from the axis (default: 5)
-    ha         - horizontal alignment (default: "center")
-    va         - vertical alignment (default: "center")
-    '''
-    fig = plt.gcf()
-    xmin = []
-    ymin = []
-    for ax in fig.axes:
-        xmin.append(ax.get_position().xmin)
-        ymin.append(ax.get_position().ymin)
-    xmin,ymin = min(xmin),min(ymin)
-    dpi = fig.dpi
-    if axis.lower() == "y":
-        rotation=90.
-        x = xmin-float(labelpad)/dpi
-        y = 0.5
-    elif axis.lower() == 'x':
-        rotation = 0.
-        x = 0.5
-        y = ymin - float(labelpad)/dpi
-    else:
-        raise Exception("Unexpected axis: x or y")
-    if label_prop is None: 
-        label_prop = dict()
-    plt.text(x,y,label,rotation=rotation,
-               transform=fig.transFigure,
-               ha=ha,va=va,
-               **label_prop)
-
 # Constants
-ref_file = '/home/sean/Documents/thesis/ref_spectra/cotton/300K.csv'
-sim_file = '/home/sean/Documents/thesis/gaussian/cellobiose/320K/10000ps/1/gdp/10000.log'
+ref_file = '/home/sean/Documents/thesis/ref_spectra/cotton/350K.csv'
+sim_file = '/home/sean/Documents/thesis/gaussian/cellobiose/350K/8000ps/1/gdp/8000.log'
 
 # plot setup
-sns.set(context='poster', style='white', font_scale=1.2)
+sns.set(context='poster', style='white', font_scale=1.3)
 
-axis_range = [210, 1650, -0.05, 1.1]
+axis_range = [210, 1650, 0, 1.1]
 
 with open(ref_file) as ref:
     lines = ref.readlines()
@@ -73,9 +36,9 @@ plt.plot(x_array, [y/max_y for y in y_array], label='Simulated', color='k')
 
 plt.text(0.1, 0.7, '300K', horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes)
 plt.gca().locator_params(axis='y', nbins=2)
-
+plt.axis(axis_range)
 plt.legend()
 plt.xlabel('Frequency (cm$^{-1}$)', fontsize=20)
-plt.ylabel('Raman Activity arb. units)')
+plt.ylabel('Normalized Spectral Intensity')
 
 plt.show()
--- ref_spectra/cotton/plot.py
+++ ref_spectra/cotton/plot.py
@@ -2,7 +2,7 @@ import seaborn as sns
 from glob import glob
 from matplotlib import pyplot as plt
 
-sns.set(context='poster', style='white', font_scale=1.2)
+sns.set(context='poster', style='white', font_scale=1.3)
 
 def parse(f):
     with open(f) as the_file:
@@ -11,7 +11,6 @@ def parse(f):
     intens = [float(line[1]) for line in lines]
     return freqs, intens
 
-fig = plt.figure(figsize=(16, 6))
 ax = plt.subplot(111)
 
 for f in sorted(glob('cotton*.csv')):
@@ -19,13 +18,10 @@ for f in sorted(glob('cotton*.csv')):
     freq, intens = parse(f)
     ax.plot(freq, intens, label=str(temp) + ' $^\circ$C')
 
-box = ax.get_position()
-ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 plt.xlim(0, 1700)
 
 plt.xlabel('Frequency (cm$^{-1}$)')
 plt.ylabel('Raman Activity (arb. units)')
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.savefig('plot.eps')
 plt.savefig('plot.png')
 plt.show()
